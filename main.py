c7a757bb7a115e41e178d32e994065bc9bff7f27
archer.li <archer.li@shopee.com>
2019-06-25 14:17:42 +0800

    loading home page

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ app = Cocoa()
 def welcome():
     return 'Minya Konga'
 
+@app.route('/static/<file_name>')
+def static(file_name):
+    with open('./static/{}'.format(file_name), 'r') as file_handler:
+        return file_handler.read()
+
 # app.run(host='0.0.0.0', port=8000)
